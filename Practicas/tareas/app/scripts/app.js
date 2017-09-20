fc94a65dbc96652f3a3800db205a1661dcc34104
guillenorway <guillermina.avigliano@gmail.com>
2017-09-20 16:20:01 -0300

    tareas 1

---
+++ Practicas/tareas/app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tareasApp
+ * @description
+ * # tareasApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tareasApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
