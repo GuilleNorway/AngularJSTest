6a3ac5a0659e48cf04db64693dfd54a44fbaca70
Guillermina Avigliano <guillermina.avigliano@gmail.com>
2017-09-22 07:42:47 -0300

    tareas v.2

---
+++ Practicas/tareasHome/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tareasHomeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tareasHomeApp
+ */
+angular.module('tareasHomeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ Practicas/tareasHome/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tareasHomeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tareasHomeApp
+ */
+angular.module('tareasHomeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
